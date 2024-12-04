aacac0dbad7c143116532129ff91ac83c5c79f9a
magnustvilde <magnustvilde@gmail.com>
2024-12-04 09:44:29 +0100

    Day 1 done

--- 2024/solution-1a.py
+++ 2024/solution-1a.py
@@ -1,12 +1,13 @@
 def configureInput() -> tuple:
-    # sort the lists
     with open('input.txt','r+') as r:
         lines = [line.strip('\n') for line in r.readlines()]
-        left = sorted([int(line[0:5]) for line in lines])
-        right = sorted([int(line[8:13]) for line in lines])
+        left = [int(line[0:5]) for line in lines]
+        right = [int(line[8:13]) for line in lines]
     return (left, right)
 
 def compareLists(list1, list2) -> int:
+    # sort the lists
+    list1, list2 = sorted(list1), sorted(list2)
     total = 0
     # compare each index
     for index in range(len(list1)):
@@ -20,7 +21,14 @@ LEFT, RIGHT =  configureInput()
 TOTAL = compareLists(LEFT, RIGHT)
 print(TOTAL)
 
+def similarityScore(list1, list2) -> int:
+    score = 0
+    for item in list1:
+        # check how many times left list index appears in right list
+        count = list2.count(item)
+        # multiply left list NUMBER by right list COUNT
+        score += count * item
+        # add to total 
+    return score
 
-# unsort left list AND right list
-
-# check how many times left list index appears in right list 
+print(similarityScore(LEFT, RIGHT))

710a54206cd341b5901c8af77d359758605fd89c
magnustvilde <magnustvilde@gmail.com>
2024-12-04 11:32:56 +0100

    Day 2, part one done.

--- 2024/2/solution-2.py
+++ 2024/2/solution-2.py
@@ -1,5 +1,5 @@
 def createReports() -> list:
-    with open('input-2.txt', 'r+') as r:
+    with open('2/second.txt', 'r+') as r:
         reports = [[int(level) for level in line.strip('\n').split(' ')] for line in r.readlines()]
     return reports
 
@@ -10,39 +10,30 @@ def validReports(reports) -> int:
         if report[1] > report[0]:
             if validASC(report):
                 count += 1
-            elif validDESC(report):
-                count += 1
+        elif validDESC(report):
+            count += 1
     return count
 
 def validASC(rep) -> bool:
     for index in range(len(rep)-1):
         diff = rep[index+1] - rep[index]
-        print(f'ASC {rep[index]} - {rep[index+1]}')
         if diff < 1 or diff > 3:
+            print(f'ASC {rep[index]} - {rep[index+1]}')
             print(f'DIFF IS NOT ASC: {diff}')
             return False
-    print('Valid asc report.')
+    # print('Valid asc report.')
     return True
 
 def validDESC(rep) -> bool:
     for index in range(len(rep)-1):
         diff = rep[index+1] - rep[index]
-        print(f'DESC {rep[index]} - {rep[index+1]}')
         if diff > -1 or diff < -3:
+            print(f'DESC {rep[index]} - {rep[index+1]}')
             print(f'DIFF IS NOT DESC: {diff}')
             return False
-    print('Valid desc report.')
+    # print('Valid desc report.')
     return True
 
-    # check validity
-        # only increasing or only decreasing
-        # check distance between numbers
-            # add to valids
-        # return number of valids
-
 REPORTS = createReports()
-print(REPORTS)
 
 print(validReports(REPORTS))
-
-# validReports(REPORTS)
